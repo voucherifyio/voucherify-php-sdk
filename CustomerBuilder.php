041663ad2104fb77ef1626afe6fd8f120bcbdd26
Jakub Reczko <contact@jakubreczko.com>
2016-06-12 18:23:20 +0200

    Added Customer object builder

---
+++ CustomerBuilder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Voucherify;
+
+class CustomerBuilder {
+
+    private $_customer;
+
+    public function __construct() {
+        $this->_customer = (object)[];
+    }
+
+    public function setName($name) {
+        $this->_customer->name = $name;
+        return $this;
+    }
+
+    public function setEmail($email) {
+        $this->_customer->email = $email;
+        return $this;
+    }
+
+    public function setDescription($description) {
+        $this->_customer->description = $description;
+        return $this;
+    }
+
+    public function setMetadata($metadata) {
+        $this->_customer->metadata = $metadata;
+        return $this;
+    }
+
+    public function build() {
+        return $this->_customer;
+    }
+}
