4bb4b320171acc7a653b4d542e63c16f87c21a7a
Maciej Andraszyk <mandraszyk@gmail.com>
2017-02-13 23:08:11 +0100

    voucherify: customers, vouchers, distributions and redemptions classes

---
+++ src/Redemptions.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Voucherify;
+
+class Redemptions
+{
+    /**
+     * @var \Voucherify\ApiClient
+     */
+    private $client;
+
+    /**
+     * @param \Voucherify\ApiClient $client
+     */
+    public function __construct($client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @param string|array $code Voucher code or array with voucher (code), customer profile and order amount
+     * @param string|null $params Provided tracking id
+     *
+     * Redeem voucher
+     *
+     * @throws \Voucherify\ClientException
+     */
+    public function redeem($code, $params = null)
+    {
+        $payload = null;
+        $options = null;
+
+        if (is_string($code)) {
+            $payload = $params;
+        }
+        elseif (is_array($code)) {
+            $payload = $code;
+            $code = $payload["voucher"];
+            unset($payload["voucher"]);
+        }
+
+        // Backward compatibility - Check if params is string -> tracking_id
+        if (is_string($params)) {
+            $options = (object)[];
+            $options->qs = ["tracking_id" => $params];
+        }
+
+        return $this->client->post("/vouchers/" . urlencode($code) . "/redemption/", $payload, $options);
+    }
+
+    /**
+     * @param array|stdClass $filter
+     *
+     * Get a filtered list of redemptions. The filter can include following properties:
+     * - limit      - number (default 100)
+     * - page       - number (default 0)
+     * - start_date - string (ISO8601 format, default is the beginning of current month)
+     * - end_date   - string (ISO8601 format, default is the end of current month)
+     * - result     - string (SUCCESS|FAILURE)
+     * - customer   - string
+     *
+     * @throws \Voucherify\ClientException
+     */
+    public function getList($filter)
+    {
+        return $this->client->get("/redemptions/", $filter);
+    }
+
+    /**
+     * @param string $code
+     *
+     * Get voucher redemption
+     *
+     * @throws \Voucherify\ClientException
+     */
+    public function getForVoucher($code)
+    {
+        return $this->client->get("/vouchers/" . urlencode($code) . "/redemption/", null);
+    }
+
+    /**
+     * @param string $redemptionId
+     * @param string|array|stdClass|null $params
+     *
+     * Rollback redemption. This operation creates a rollback entry in voucher's redemption history (`redemption.redemption_entries`)
+     * and gives 1 redemption back to the pool (decreases `redeemed_quantity` by 1).
+     *
+     * @throws \Voucherify\ClientException
+     */
+    public function rollback($redemptionId, $params = null)
+    {
+        $payload = (object)[];
+        $options = (object)[];
+
+        if (is_string($params)) {
+            $options->qs->reason = $params;
+        }
+        elseif (is_object($params)) {
+
+            if (isset($params->reason)) {
+                $options->qs->reason = $params->reason;
+            }
+
+            if (isset($params->$tracking_id)) {
+                $options->qs->tracking_id = $params->tracking_id;
+            }
+
+            $payload->customer = $params->customer;
+        }
+        elseif (is_array($params)) {
+
+            if (isset($params["reason"])) {
+                $options->qs->reason = $params["reason"];
+            }
+
+            if (isset($params["tracking_id"])) {
+                $options->qs->tracking_id = $params["tracking_id"];
+            }
+
+            $payload->customer = $params["customer"];
+        }
+
+        return $this->client->post("/redemptions/" . urlencode($redemptionId) . "/rollback/", $payload, $options);
+    }
+}
---
+++ src/Vouchers.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Voucherify;
+
+class Vouchers
+{
+    /**
+     * @var \Voucherify\ApiClient
+     */
+    private $client;
+
+    /**
+     * @param \Voucherify\ApiClient $client
+     */
+    public function __construct($client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @param stdClass $voucher
+     *
+     * Create voucher.
+     *
+     * @throws \Voucherify\ClientException
+     */
+    public function create($voucher)
+    {
+        if (isset($voucher->code)) {
+            return $this->client->post("/vouchers/" . urlencode($voucher->code), $voucher, null);
+        }
+        else {
+            return $this->client->post("/vouchers/", $voucher, null);
+        }
+    }
+
+    /**
+     * @param string $code
+     *
+     * Get voucher details.
+     *
+     * @throws \Voucherify\ClientException
+     */
+    public function get($code)
+    {
+        return $this->client->get("/vouchers/" . urlencode($code), null);
+    }
+
+    /**
+     * @param array|stdClass $voucher
+     *
+     * Update voucher.
+     *
+     * @throws \Voucherify\ClientException
+     */
+    public function update($voucher)
+    {
+        $code = "";
+
+        if (is_array($voucher)) {
+            $code = $voucher["code"];
+        }
+        elseif (is_object($voucher)) {
+            $code = $voucher->code;
+        }
+
+        return $this->client->put("/vouchers/" . urlencode($code), $voucher, null);
+    }
+
+    /**
+     * @param string $code
+     * @param boolean|null $force
+     *
+     * Delete voucher by code.
+     *
+     * @throws \Voucherify\ClientException
+     */
+    public function delete($code, $force = null)
+    {
+        $options = (object)[];
+        $options->qs = [ "force" => ($force ? "true" : "false") ];
+
+        return $this->client->delete("/vouchers/" . urlencode($code), null, $options);
+    }
+
+    /**
+     * @param array|stdClass $filter
+     *
+     * Get a filtered list of vouchers. The filter can include following properties:
+     * - limit      - number (default 10)
+     * - page       - number (default 0)
+     * - campaign   - string
+     * - category   - string
+     *
+     * @throws \Voucherify\ClientException
+     */
+    public function getList($filter)
+    {
+        return $this->client->get("/vouchers/", $filter);
+    }
+
+    /**
+     * @param string $code
+     *
+     * Enable voucher with given code.
+     *
+     * @throws \Voucherify\ClientException
+     */
+    public function enable($code)
+    {
+        return $this->client->post("/vouchers/" . urlencode($code) . "/enable", null, null);
+    }
+
+    /**
+     * @param string $code
+     *
+     * Disable voucher with given code.
+     *
+     * @throws \Voucherify\ClientException
+     */
+    public function disable($code)
+    {
+        return $this->client->post("/vouchers/" . urlencode($code) . "/disable", null, null);
+    }
+}
